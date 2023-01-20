3c132fbc833989f5064bee5650de1992ea3350b2
Roop <roop9540@gmail.com>
2023-01-20 11:42:13 +0530

    Working Amazone

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
+// import React from 'react';
+import ReactDOM from "react-dom";
 // import index from './index.css'
 import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+window.React = React
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+const root = ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
